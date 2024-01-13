dc3db9ea48acc76bf8e936bc2d2900947666e428
jxsh2 <92442082+jxsh2@users.noreply.github.com>
2024-01-13 23:56:16 +0800

    Update test_lexical.py

--- test_lexical.py
+++ test_lexical.py
@@ -9,7 +9,7 @@ def tokenize_and_categorize(input_program):
     operators = set(["++", "-", "=", "*", "/", "%", "--", "<=", ">="])
     delimeters = set(["[", "]", "(", ")", "{", "}", " ", "//", "/*", "*/" , "}", "'", '"', ";"])
     numerals = set("0123456789")
-    general = set(["ERROR", "NUM", "EOF", "IDENTIFIER", "CHAR", "STRING"])
+    general = set(["ERROR", "EOF", ])
 
     # Split input_program into tokens
     input_program_tokens = input_program.split()
@@ -130,16 +130,8 @@ def tokenize_and_categorize(input_program):
         elif token in general:
             if token == "ERROR":
                 ctoken = "ERROR"
-            elif token == "NUM":
-                ctoken = "NUM"
             elif token == "EOF":
                 ctoken = "EOF" 
-            elif token == "IDENTIFIER":    
-                ctoken = "IDENTIFIER"
-            elif token == "CHAR":    
-                ctoken = "CHAR"
-            elif token == "STRING":    
-                ctoken = "STRING"
             else:
                 ctoken = "INVALID"
                                                           

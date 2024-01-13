ca95f9a8c02fa19b3d91b67aec469fc0cbe0adec
jxsh2 <92442082+jxsh2@users.noreply.github.com>
2024-01-13 15:19:57 +0800

    Update lexical.py

--- lexical.py
+++ lexical.py
@@ -1,44 +1,47 @@
-#lexical analyzer
-#library for regular expression
-import re
 #define tokens
 def tokenize_and_categorize(input_program):
-    # Define regular expressions for different token types
+    # Define sets for different token types
     keywords = set(["auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else",
                     "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short",
                     "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void",
                     "volatile", "while", "string", "class", "struc", "include"])
     
-    operators = re.compile(r"(\++)|(-)|(=)|(\*)|(/)|(%)|(--)|(<=)|(>=)")
-    numerals = re.compile(r"^\d+$")
-    special_characters = re.compile(r"[\[@&~!#$\^\|{}\]:;<>?,\.']|\(\)|\(|\)|{}|\[\]|\"")
-    identifiers = re.compile(r"^[a-zA-Z_][a-zA-Z0-9_\-]*$")
-    headers = re.compile(r"([a-zA-Z]+\.[h])")
+    operators = set(["++", "-", "=", "*", "/", "%", "--", "<=", ">="])
+    special_characters = set(r"[@&~!#$\^|{}]:;<>,.'()")
+    numerals = set("0123456789")
 
     # Tokenize the input program
-    input_program_tokens = re.findall(r'\b\w+\b', input_program)
+    input_program_tokens = input_program.split()
+
+    # Print the header for the table
+    print("{:<20} {:<30}".format("Lexeme", "Token"))
+    print("="*50)
 
     # To Categorize The Tokens
     for token in input_program_tokens:
+        category = ""
         if token in keywords:
-            print(token, "-------> Keyword/Reserved Word")
+            category = "Keyword/Reserved Word"
         elif token.lower() in ["int", "char", "double", "float", "long", "short", "signed", "unsigned", "void"]:
-            print(token, "-------> Data Type")
-        elif operators.match(token):
-            print(token, "-------> Operator")
-        elif numerals.match(token):
-            print(token, "-------> Numeral")
-        elif special_characters.match(token):
-            print(token, "-------> Special Character/Symbol")
-        elif identifiers.match(token):
-            print(token, "-------> Identifier")
-        elif headers.match(token):
-            print(token, "-------> Header")
+            category = "Data Type"
+        elif token in operators:
+            category = "Operator"
+        elif all(char in numerals for char in token):
+            category = "Numeral"
+        elif all(char in special_characters for char in token):
+            category = "Special Character/Symbol"
+        elif token.isidentifier():
+            category = "Identifier"
+        elif '.' in token and token.split('.')[0].isalpha() and token.split('.')[1] == 'h':
+            category = "Header"
         elif token == ';':
-            print(token, "-------> Terminator (End of Statement)")
+            category = "Terminator (End of Statement)"
             break  # Exit the loop after encountering the terminator
         else:
-            print("Unknown Value")
+            category = "Unknown Value"
+
+        # Print the token and its category in a formatted way
+        print("{:<20} {:<30}".format(token, category))
 
 # Example usage
 input_program = input("Enter Your Code: ")

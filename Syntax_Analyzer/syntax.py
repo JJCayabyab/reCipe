3a697291c1a123628ee2bfdad073d79be8890eb7
jxsh2 <92442082+jxsh2@users.noreply.github.com>
2024-02-07 16:58:12 +0800

    Update syntax.py

--- Syntax_Analyzer/syntax.py
+++ Syntax_Analyzer/syntax.py
@@ -1,4 +1,3 @@
-
 from token_dict import KEY
 import string
 
@@ -19,131 +18,132 @@ TT_UN = "_"
 BOOL = {"TR", "FL"}
 VALUES = {"IL", "FL", "DL"}
 
+# AST Node classes
+class DataTypeNode:
+    def __init__(self, datatype):
+        self.datatype = datatype
+
+class IdentifierNode:
+    def __init__(self, name):
+        self.name = name
+
+class AssignmentNode:
+    def __init__(self, identifier, value):
+        self.identifier = identifier
+        self.value = value
+
+class DeclarationNode:
+    def __init__(self, datatype, identifiers, assignments=None):
+        self.datatype = datatype
+        self.identifiers = identifiers
+        self.assignments = assignments if assignments is not None else []
+
+class ValueNode:
+    def __init__(self, values) :
+        self.values = values
+
 # Parsing functions
-
 class Parser:
-    
-#################################### hold value of token
-
     def __init__(self, tokens):
         self.tokens = tokens
         self.index = -1
         self.advance()
-        
-#################################### move to next token
 
     def advance(self):
         self.index += 1
         if self.index < len(self.tokens):
             self.current = self.tokens[self.index]
+            print("Current token:", self.current)
         return self.current
-    
-#################################### data types    
-#CFG: <DT> ::= INT | CHAR | STR | FLOAT | DOUBLE | BOOL
 
     def data_types(self):
         if self.current in KEY:
-            typespecifier = self.current
+            datatype = self.current
             self.advance()
-            return typespecifier
+            return DataTypeNode(datatype)
         else:
             raise SyntaxError("Expected data type keyword but found: " + str(self.current))
-        
-#################################### identifiers 
-#CFG: <IDEN> ::= <ALPHABET> (<ALPHABET> | <DIGITS> | "_")*| "_" (<ALPHABET> | <DIGITS> | "_")*
 
     def identifiers(self):
-        if self.current is TT_ID:
+        if self.current == TT_ID:
             identifier = self.current
             self.advance()
-        return identifier
-
-#################################### idetifiers list e.g a, b, c
-#CFG: <IDENLIST> ::= <IDEN>, <IDENLIST>*
-
-    def iden_list(self):
-        idenlist = []
-        if self.current == TT_ID:
-            idenlist.append(self.current)
-            self.advance()
-
-            while self.current == TT_COM:
-                self.advance()
-                if self.current == TT_ID:
-                    idenlist.append(self.current)
-                    self.advance()
-                else:
-                    raise SyntaxError("Expected identifier after comma")
-
-            if self.current != TT_SEMI:  # Check for semicolon after the identifier list
-                raise SyntaxError("Expected ';' after identifier list")
-
+            return IdentifierNode(identifier)
         else:
-            raise SyntaxError("Expected identifier at the beginning of identifier list")
-        
-        return idenlist
-
-#################################### assigment statements
-#CFG: <ASS> ::= <IDEN> = <VAL>
+            raise SyntaxError("Expected identifier but found: " + str(self.current))
 
     def assign(self):
         if self.current == TT_ID:
             identifier = self.current
             self.advance()
-            
             if self.current == TT_ASS:
-                self.advance()  # Move past the '=' token
-                
-                if self.current in VALUES:
+                self.advance()
+                if self.current in [DIGITS, LETTERS_DIGITS, BOOL, TT_ID]:
                     value = self.current
                     self.advance()
-                    return identifier, value
+                    return AssignmentNode(identifier, value)
                 else:
-                    raise SyntaxError("Expected value after '='")
+                    raise SyntaxError("Expected value after '=' but found: " + str(self.current))
             else:
-                raise SyntaxError("Expected '=' after identifier")
+                raise SyntaxError("Expected '=' after identifier but found: " + str(self.current))
         else:
-            raise SyntaxError("Expected identifier")
+            raise SyntaxError("Expected identifier but found: " + str(self.current))
         
-#################################### assigment statements list
-#CFG: <ASS_STMNT> ::= <ASS> | <ASS>, <ASS_STMNT>
-
-    def assig_list(self):
-        ass_list = [self.assign()]  # Parse the first assignment
-        while self.current == TT_COM:  # Check for comma
-            self.advance()  # Move past the comma
-            ass_list.append(self.assign())  # Parse next assignment
-        return ass_list
-    
-####################################  values
-#CFG: <VAL> ::= <DIGITS> | <STRING> | <BOOLEAN> | <IDEN>
-
     def values(self):
         if self.current in [DIGITS, LETTERS_DIGITS, BOOL, TT_ID]:
             value = self.current
             self.advance()  # Move to the next token
-            return value
+            return ValueNode (value)
         else:
             raise SyntaxError("Invalid value encountered: " + str(self.current))
-        
-#################################### 
-# DECLARATIONS
-#################################### 
-#CFG: <DEC_STATEMENT> ::= <DT> <IDEN> ; | <DT> <IDEN> , <IDENLIST>* ; | <DT> <ASS_STMNT> ; | <DT> <IDENLIST> <ASS_STMNT>* ;
-    
- 
+
+    def declarations(self):
+        datatype_node = self.data_types()
+        identifier_node = self.identifiers()
+
+        if self.current == TT_SEMI:
+            self.advance()
+            return DeclarationNode(datatype_node, [identifier_node])
+
+        elif self.current == TT_COM:
+            self.advance()
+            identifiers = [identifier_node]
+            while self.current == TT_ID:
+                identifier_node = self.identifiers()
+                identifiers.append(identifier_node)
+                if self.current == TT_COM:
+                    self.advance()
+                else:
+                    break
+
+            if self.current == TT_SEMI:
+                self.advance()
+                return DeclarationNode(datatype_node, identifiers)
+            else:
+                raise SyntaxError("Expected ';' after identifiers but found: " + str(self.current))
+
+        elif self.current == TT_ASS:
+            assignments = [self.assign()]
+            while self.current == TT_COM:
+                self.advance()
+                assignments.append(self.assign())
+            if self.current == TT_SEMI:
+                self.advance()
+                return DeclarationNode(datatype_node, [], assignments)
+            else:
+                raise SyntaxError("Expected ';' after assignments but found: " + str(self.current))
+        else:
+            raise SyntaxError("Invalid syntax at declaration: " + str(self.current))
+
 
 # Function to read tokens from a file
 def read_tokens_from_file(file_path):
-    
     with open(file_path, 'r') as file:
         tokens = file.read().split(":")
     return tokens
 
 # Example usage with a text file
-file_path = 'lexer.txt'  # Replace 'lexer.txt' with the path to your output file
+file_path = 'lexer.txt'
 tokens = read_tokens_from_file(file_path)
 parser = Parser(tokens)
-print(parser.DEC())
-
-
+ast = parser.declarations()
